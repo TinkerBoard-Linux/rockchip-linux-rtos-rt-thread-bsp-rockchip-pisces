95f6d1e619d15cf098de49d05e9f3f5d93295b21
tonyzheng-rockchip <tony.zheng@rock-chips.com>
2020-01-06 10:22:36 +0800

    [Pisces]: applications: Add config for dynamic load apps
    Change-Id: I7a6b0cd015295d2ac8979ceb4612b03603f7051d
    Signed-off-by: tonyzheng-rockchip <tony.zheng@rock-chips.com>

--- applications/olpc_demo/olpc_main.c
+++ applications/olpc_demo/olpc_main.c
@@ -58,27 +58,35 @@ RTM_EXPORT(olpc_main_event);
  *
  **************************************************************************************************
  */
-
+#ifdef OLPC_OVERLAY_ENABLE
 /**
  * Get firmware from AP.
  */
 static rt_err_t olpc_firmware_request(rt_uint32_t id)
 {
-#ifdef OLPC_OVERLAY_ENABLE
+    rt_err_t ret = RT_ERROR;
     FIRMWARE_REQ_PARAM  Param;
     FIRMWARE_REQ_PARAM *pParam = &Param;
 
     rt_memset(pParam, 0, sizeof(FIRMWARE_REQ_PARAM));
     pParam->id   = id;
     pParam->type = SEGMENT_ALL;
-    olpc_ap_command(FIRMWARE_DOWNLOAD_REQ, pParam, sizeof(FIRMWARE_REQ_PARAM));
-#endif
-    return RT_EOK;
+    ret = olpc_ap_command(FIRMWARE_DOWNLOAD_REQ, pParam, sizeof(FIRMWARE_REQ_PARAM));
+    if (ret == RT_EOK)
+    {
+        if (pParam->info.CodeImageLength == 0)
+        {
+            ret = RT_ERROR;
+        }
+    }
+
+    return ret;
 }
+#endif
 
 #if defined(RT_USING_CUSTOM_DLMODULE)
 /**
- * Get dlmodule from AP.
+ * dlmodule request hook.
  */
 static rt_uint8_t *olpc_dlmodule_request(const char *name)
 {
@@ -106,23 +114,52 @@ static rt_uint8_t *olpc_dlmodule_request(const char *name)
     return pParam->buf;
 }
 
+/**
+ * dlmodule release hook.
+ */
 static rt_err_t olpc_dlmodule_release(rt_uint8_t *param)
 {
     rt_dma_free_large(param);
 
     return RT_EOK;
 }
-#endif
 
+/**
+ * Get dlmodule from & start dlmodule.
+ */
 static int olpc_dlmodule_exec(const char *pgname)
 {
-#if defined(RT_USING_CUSTOM_DLMODULE)
+    struct rt_dlmodule *module;
     struct rt_dlmodule_ops ops;
     ops.load = olpc_dlmodule_request;
     ops.unload = olpc_dlmodule_release;
-    dlmodule_exec_custom(pgname, pgname, rt_strlen(pgname), &ops);
+    module = dlmodule_exec_custom(pgname, pgname, rt_strlen(pgname), &ops);
+    if (module)
+    {
+        return RT_EOK;
+    }
+    return RT_ERROR;
+}
 #endif
-    return RT_EOK;
+
+/**
+ * olpc_demo app init.
+ */
+static void olpc_apps_init(void)
+{
+#if defined(OLPC_APP_CLOCK_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_CLOCK);
+#elif defined(OLPC_APP_EBOOK_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_EBOOK);
+#elif defined(OLPC_APP_BLOCK_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_BLOCK);
+#elif defined(OLPC_APP_SNAKE_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_SNAKE);
+#elif defined(OLPC_APP_NOTE_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_NOTE);
+#elif defined(OLPC_APP_XSCREEN_ENABLE)
+    rt_event_send(olpc_main_event, EVENT_APP_XSCREEN);
+#endif
 }
 
 /*
@@ -166,24 +203,11 @@ static void olpc_main_thread(void *p)
 
     olpc_main_event = rt_event_create("olpcmain_event", RT_IPC_FLAG_FIFO);
     RT_ASSERT(olpc_main_event != RT_NULL);
-#if 0
-#if defined(OLPC_APP_CLOCK_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_CLOCK);
-#elif defined(OLPC_APP_EBOOK_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_EBOOK);
-#elif defined(OLPC_APP_BLOCK_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_BLOCK);
-#elif defined(OLPC_APP_SNAKE_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_SNAKE);
-#elif defined(OLPC_APP_NOTE_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_NOTE);
-#elif defined(OLPC_APP_XSCREEN_ENABLE)
-    rt_event_send(olpc_main_event, EVENT_APP_XSCREEN);
-#endif
-#endif
+    olpc_apps_init();
 
+#if defined(RT_USING_CUSTOM_DLMODULE)
     olpc_dlmodule_exec("hello.mo");
-    olpc_dlmodule_exec("ebook.mo");
+#endif
 
     while (1)
     {
@@ -196,49 +220,145 @@ static void olpc_main_thread(void *p)
         if (event & EVENT_APP_CLOCK)
         {
 #if defined(OLPC_APP_CLOCK_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_CLOCK);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("clock.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_CLOCK);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_clock_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_clock_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_clock error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
         else if (event & EVENT_APP_EBOOK)
         {
 #if defined(OLPC_APP_EBOOK_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_EBOOK);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("ebook.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_EBOOK);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_ebook_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_ebook_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_ebook error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
         else if (event & EVENT_APP_BLOCK)
         {
 #if defined(OLPC_APP_BLOCK_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_BLOCK);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("block.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_BLOCK);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_block_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_block_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_block error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
         else if (event & EVENT_APP_SNAKE)
         {
 #if defined(OLPC_APP_SNAKE_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_SNAKE);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("snake.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_SNAKE);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_snake_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_snake_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_snake error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
         else if (event & EVENT_APP_NOTE)
         {
 #if defined(OLPC_APP_NOTE_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_NOTE);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("note.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_NOTE);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_note_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_note_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_note error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
         else /*if (event & EVENT_APP_XSCREEN)*/
         {
 #if defined(OLPC_APP_XSCREEN_ENABLE)
-            olpc_firmware_request(SEGMENT_ID_OLPC_XSCREEN);
+            ret = RT_ERROR;
+#if defined (OLPC_DLMODULE_ENABLE)
+            ret = olpc_dlmodule_exec("xscreen.mo");
+#elif defined (OLPC_OVERLAY_ENABLE)
+            ret = olpc_firmware_request(SEGMENT_ID_OLPC_XSCREEN);
+            if (ret == RT_EOK)
+            {
+                ret = olpc_xscreen_app_init();
+            }
+#elif defined (OLPC_STATICLD_ENABLE)
             ret = olpc_xscreen_app_init();
-            RT_ASSERT(ret == RT_EOK);
+#endif
+            if (ret)
+            {
+                rt_kprintf("olpc_start_app_xscreen error!!!\n");
+                olpc_apps_init();
+            }
+#else
+            olpc_apps_init();
 #endif
         }
     }
